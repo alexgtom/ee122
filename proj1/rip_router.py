9fb83b700e40f01a2668846c0b86412bf1fb9255
Alex Tom <alexgtom@berkeley.edu>
2013-10-03 13:08:11 -0700

    Got simple ping working

--- proj1/rip_router.py
+++ proj1/rip_router.py
@@ -2,6 +2,8 @@ from sim.api import *
 from sim.basics import *
 
 class DistanceTable(object):
+    INFINITY = 100
+
     def __init__(self):
         self.distance = {}
 
@@ -11,19 +13,107 @@ class DistanceTable(object):
         self.distance[dst][via] = distance
 
     def get(self, dst, via=None):
+        try:
+            self.distance[dst]
+        except KeyError:
+            return self.INFINITY
+
         if via:
             return self.distance[dst][via]
         else:
             return min(self.distance[dst].values())
 
+    def get_via(self, dst):
+        d = self.distance[dst]
+        return min(d, key=d.get)
+
+    def values(self):
+        return self.distance.values()
+
+    def keys(self):
+        return self.distance.keys()
+
+    def __contains__(self, key):
+        return key in self.distance
+
+    def __str__(self):
+        s = ""
+        s += "dst: distances\n"
+        for key in self.distance:
+            s += str(key) + " :::: " + str(self.distance[key]) + "\n"
+        return s
+
+    def __len__(self):
+        return len(self.distance)
+
+class PortTable(object):
+    def __init__(self):
+        self.t = {}
+
+    def get_port(self, host):
+        return self.t[host]
+
+    def get_host(self, port):
+        for key, value in self.t.iteritems():
+            if value == port:
+                return key
+        raise KeyError
+
+    def set(self, host, port):
+        self.t[host] = port
+
+    def del_host(self, host):
+        del self.t[host]
+
+    def values(self):
+        return self.t.values()
 '''
 Create your RIP router in this file.
 '''
 class RIPRouter (Entity):
     def __init__(self):
-        # Add your code here!
-        pass
+        self.dt = DistanceTable()
+        self.port_table = PortTable()
 
     def handle_rx (self, packet, port):
-        # Add your code here!
-        raise NotImplementedError
+        if isinstance(packet, DiscoveryPacket):
+            self.handle_discovery_packet(packet, port)
+        elif isinstance(packet, RoutingUpdate):
+            self.handle_routing_update(packet, port)
+        else:
+            # handle other packet
+            dst = self.dt.get_via(packet.dst)
+            self.send(packet, self.port_table.get_port(dst))
+
+
+    def handle_discovery_packet(self, packet, port):
+        if packet.is_link_up:
+            # if link is up, set distance to dst to infinity
+            self.port_table.set(packet.src, port)
+            self.dt.set(packet.src, packet.src, 1)
+
+            # if there is a new minimum distnace to a destination,
+            # add it to the routing update
+            routing_update = RoutingUpdate()
+            for dst in self.dt.keys():
+                routing_update.add_destination(dst, self.dt.get(dst))
+            self.send(routing_update, port=self.port_table.values())
+
+        else:
+            # if link is down, set distance to dst to infinity
+            self.dt.set(packet.src, packet.dst, DistanceTable.INFINITY)
+            self.port_table.del_host(packet.src)
+
+    def handle_routing_update(self, packet, port):
+        routing_update = RoutingUpdate()
+        for dst in packet.all_dests():
+            if dst != self:
+                packet_src = self.port_table.get_host(port)
+                new_dist = self.dt.get(packet_src) + packet.get_distance(dst)
+                self.dt.set(dst, packet_src, new_dist)
+
+                if new_dist < self.dt.get(dst):
+                    routing_update.add_destination(dst, new_dist)
+
+        if len(packet.all_dests()) > 0:
+            self.send(routing_update, port=self.port_table.values())
--- proj1/tests.py
+++ proj1/tests.py
@@ -1,10 +1,13 @@
 import unittest
 
-from rip_router import DistanceTable
+from rip_router import DistanceTable, RIPRouter
+from sim.basics import BasicHost, DiscoveryPacket, Ping, RoutingUpdate
+from hub import Hub
 
 class DistanceTableTestCase(unittest.TestCase):
     def setUp(self):
         self.dt = DistanceTable()
+
     def testGetAndSet(self):  ## test method names begin 'test*'
         self.dt.set("A", "B", 2)
         self.dt.set("A", "C", 1)
@@ -12,5 +15,115 @@ class DistanceTableTestCase(unittest.TestCase):
         self.assertEquals(1, self.dt.get("A", "C"))
         self.assertEquals(2, self.dt.get("A", "B"))
 
-if __name__ == '__main__':
-    unittest.main()
+        # get invalid destination
+        self.assertEquals(DistanceTable.INFINITY, self.dt.get("D"))
+
+        # get_via
+        self.assertEquals("C", self.dt.get_via("A"))
+
+    def testIn(self):
+        self.assertFalse("A" in self.dt)
+        self.dt.set("A", "B", 2)
+        self.assertTrue("A" in self.dt)
+
+
+def createRIPRouterMock(send_assert=None, handle_rx_assert=None):
+    class RIPRouterMock(RIPRouter):
+        def send(self, packet, port=None, flood=False):
+            if send_assert is not None:
+                send_assert(packet, port, flood)
+            else:
+                super(RIPRouterMock, self).send(packet, port, flood)
+
+        def handle_rx(self, packet, port):
+            if handle_rx_assert is not None:
+                handle_rx_assert(packet, port)
+            else:
+                super(RIPRouterMock, self).handle_rx(packet, port)
+    return RIPRouterMock
+
+
+def createBasicHostMock(send_assert=None, handle_rx_assert=None):
+    class BasicHostMock(BasicHost):
+        def send(self, packet, port=None, flood=False):
+            if send_assert is not None:
+                send_assert(packet, port, flood)
+            else:
+                super(BasicHostMock, self).send(packet, port, flood)
+
+        def handle_rx(self, packet, port):
+            if handle_rx_assert is not None:
+                handle_rx_assert(packet, port)
+            else:
+                super(BasicHostMock, self).handle_rx(packet, port)
+    return BasicHostMock 
+
+
+class RIPRouterBasicTest(unittest.TestCase):
+    """
+    Network:
+        s1 -- s2
+         |     |
+        h1    h2
+    """
+    def setUp(self):
+        self.h1 = BasicHost.create('h1')
+        self.h2 = BasicHost.create('h2')
+
+        self.s1 = RIPRouter.create('s1')
+        self.s2 = RIPRouter.create('s2')
+
+        # connect switches to host
+        self.s1.linkTo(self.h1)
+        self.s2.linkTo(self.h2)
+        
+        # connect switches together
+        self.s1.linkTo(self.s2)
+
+        dp_h1 = DiscoveryPacket(self.h1, 1)
+        dp_h2 = DiscoveryPacket(self.h2, 1)
+        dp_s1 = DiscoveryPacket(self.s1, 1)
+        dp_s2 = DiscoveryPacket(self.s2, 1)
+        
+        # send discovery packet between h1 -- s1 on port 0
+        self.h1.handle_rx(dp_s1, 0)
+        self.s1.handle_rx(dp_h1, 0)
+
+        # send discovery packet between h2 -- s2 on port 0
+        self.h2.handle_rx(dp_s2, 0)
+        self.s2.handle_rx(dp_h2, 0)
+
+        # send discovery packet between s1 -- s2 on port 1
+        self.s1.handle_rx(dp_s2, 1)
+        self.s2.handle_rx(dp_s1, 1)
+
+        self.entities = [self.h1, self.h2, self.s1, self.s2]
+
+    def tearDown(self):
+        for entity in self.entities:
+            entity.remove()
+
+    def testSetUp(self):
+        # test setup
+        self.assertEquals(1, self.s1.dt.get(self.h1))
+        self.assertEquals(1, self.s1.dt.get(self.s2))
+        self.assertEquals(2, len(self.s1.dt))
+
+        self.assertEquals(1, self.s2.dt.get(self.h2))
+        self.assertEquals(1, self.s2.dt.get(self.s1))
+        self.assertEquals(2, len(self.s2.dt))
+        
+        self.h1.ping(self.h2)
+
+    def testHandleRoutingUpdate(self):
+        # s2 sends routing update to s1
+        routing_update = RoutingUpdate()
+        for dst in self.s2.dt.keys():
+            routing_update.add_destination(dst, self.s2.dt.get(dst))
+        self.s1.handle_rx(routing_update, 1)
+        
+        # assert distance table is updated
+        self.assertEquals(1, self.s1.dt.get(self.h1))
+        self.assertEquals(1, self.s1.dt.get(self.s2))
+        self.assertEquals(2, self.s1.dt.get(self.h2))
+        self.assertEquals(3, len(self.s1.dt))
