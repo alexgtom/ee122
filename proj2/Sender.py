0537b0340eff378b6d802b930839be93e8fec1d4
Alex Tom <alexgtom@berkeley.edu>
2013-10-25 14:07:38 -0700

    Added Window and other helper functions

--- proj2/Sender.py
+++ proj2/Sender.py
@@ -1,5 +1,7 @@
 import sys
 import getopt
+import time
+import socket
 
 import Checksum
 import BasicSender
@@ -7,15 +9,71 @@ import BasicSender
 '''
 This is a skeleton sender class. Create a fantastic transport protocol here.
 '''
+
+class Window(object):
+    def __init__(self, window_size=5):
+        self.window_size = window_size
+        self.packets_dict = {}
+
+    def set(self, seqno, packet):
+        if len(self.packets_dict) == self.window_size:
+            raise Exception("Too many packets in window.")
+        else:
+            self.packets_dict[seqno] = packet
+
+    def get(self, seqno):
+        return self.packets_dict[seqno]
+
+    def remove(self, seqno):
+        packet = self.get(seqno)
+        del self.packets_dict[seqno]
+        return packet
+
+    def __contains__(self, seqno):
+        return seqno in self.packets_dict
+
 class Sender(BasicSender.BasicSender):
+    # packet size - message type - seqno - checksum - number of seperators
+    PACKET_SIZE = 1472
+    CHUNK_SIZE = PACKET_SIZE - 5 - 10 - 10 - 3 
+
     def __init__(self, dest, port, filename, debug=False):
         super(Sender, self).__init__(dest, port, filename, debug)
+        self.window = Window()
+        self.current_seqno = 0
 
     # Main sending loop.
     def start(self):
-        raise NotImplementedError
+        while True:
+            try:
+                message, address = self.receive()
+                msg_type, seqno, data, checksum = self._split_message(message)
+                try:
+                    seqno = int(seqno)
+                except:
+                    raise ValueError
+                if debug:
+                    print "%s %d %s %s" % (msg_type, seqno, data, checksum)
+                if Checksum.validate_checksum(message):
+                    self.handle_ack(seqno)
+                elif self.debug:
+                    print "checksum failed: %s" % message
+
+                if time.time() - self.last_cleanup > self.timeout:
+                    self._cleanup()
+            except socket.timeout:
+                self.handle_timeout()
+            except (KeyboardInterrupt, SystemExit):
+                exit()
+            except ValueError, e:
+                if self.debug:
+                    print e
+                pass # ignore
 
     def handle_timeout(self):
+        pass
+
+    def handle_ack(self, ack):
         pass
 
     def handle_new_ack(self, ack):
@@ -27,6 +85,50 @@ class Sender(BasicSender.BasicSender):
     def log(self, msg):
         if self.debug:
             print msg
+
+    def get_file_chunk(self):
+        """ 
+        returns a chunk in the file of size CHUNK_SIZE, returns None
+        if there are no more chunks left 
+        """
+        chunk = self.infile.read(self.CHUNK_SIZE)
+        if len(chunk) == 0:
+            return None
+        return chunk
+
+    def resend(self, seqno, address=None):
+        """ Resends a packet based on it's seqno """
+        super(Sender, self).send(self.window.get(seqno), address)
+
+    def send_start(self, **kwargs):
+        return self.send("start", **kwargs)
+
+    def send_data(self, **kwargs):
+        return self.send("data", **kwargs)
+
+    def send_end(self, **kwargs):
+        return self.send("end", **kwargs)
+
+    def send(self, msg_type, seqno=None, data=None, address=None):
+        if data == None:
+            data = self.get_file_chunk()
+        if seqno == None:
+            seqno = self.current_seqno
+            self.current_seqno += 1
+        
+        # create packet
+        packet = self.make_packet(msg_type, seqno, data)
+        assert len(self.packet) <= self.PACKET_SIZE
+
+        # add to window
+        self.window.set(seqno, packet)
+
+        # send packet
+        super(Sender, self).send(packet, address)
+
+        return (seqno, packet)
+
+
 
 '''
 This will be run if you run this script from the command line. You should not
