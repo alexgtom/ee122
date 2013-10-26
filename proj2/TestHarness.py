3a8b9acdcd20daf26b28b0db8b98e92da35ecfbe
Alex Tom <alexgtom@berkeley.edu>
2013-10-25 17:56:23 -0700

    Added tests to TestHarness

--- proj2/TestHarness.py
+++ proj2/TestHarness.py
@@ -15,9 +15,24 @@ you want to run. The tests automatically register themselves with the
 forwarder, so they will magically be run.
 """
 def tests_to_run(forwarder):
-    from tests import BasicTest, RandomDropTest
+    from tests import BasicTest, RandomDropTest, CorruptTest, DelayTest, \
+    DropFirstPacketsTest, DropStartAckTest, NonAckTest, RandomCorruptTest, \
+    RandomDelayTest, RandomDuplicateTest, RandomReorderTest, StartAndEndTest
+    
     BasicTest.BasicTest(forwarder, "README")
     RandomDropTest.RandomDropTest(forwarder, "README")
+    CorruptTest.CorruptTest(forwarder, "README")
+    DelayTest.DelayTest(forwarder, "README")
+    DropFirstPacketsTest.DropFirstPacketsTest(forwarder, "README")
+    DropStartAckTest.DropStartAckTest(forwarder, "README")
+    NonAckTest.NonAckTest(forwarder, "README")
+
+    RandomCorruptTest.RandomCorruptTest(forwarder, "README")
+    RandomDelayTest.RandomDelayTest(forwarder, "README")
+
+    RandomDuplicateTest.RandomDuplicateTest(forwarder, "README")
+    RandomReorderTest.RandomReorderTest(forwarder, "README")
+    StartAndEndTest.StartAndEndTest(forwarder, "README")
 
 """
 Testing is divided into two pieces: this forwarder and a set of test cases in
