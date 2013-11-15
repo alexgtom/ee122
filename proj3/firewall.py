f7517c7046f99fa4a0d8869c29cc50d0eb9f4c6c
jasonjia93 <jason.jia1993@gmail.com>
2013-11-15 01:28:42 -0800

    Updated firewall
    Changed how packets are parse and protocols are handles
    Still have to implement how rules are handles

--- proj3/firewall.py
+++ proj3/firewall.py
@@ -41,16 +41,21 @@ class Firewall:
         with open(config['rule'], 'r') as f:
             # strip trailing and beginning whitespace from lines
             lines = [l.strip() for l in f.readlines()]
-
             # strip out blank lines
             lines = [l for l in lines if len(l) > 0]
-
             # strip out comments
             lines = [l for l in lines if l[0] != "%"]
 
             self.rules = lines
 
         # TODO: Load the GeoIP DB ('geoipdb.txt') as well.
+        self.geoIP = []
+        ips = [l.rstrip('\n') for l in open("geoipdb.txt")]
+        ips = [l for l in ips if len(l) > 0]
+        ips = [l for l in ips if l[0] != '%']
+
+        self.geoIP = ips
+
         # TODO: Also do some initialization if needed.
 
     def handle_timer(self):
@@ -61,6 +66,142 @@ class Firewall:
     # @pkt: the actual data of the IPv4 packet (including IP header)
     def handle_packet(self, pkt_dir, pkt):
         # TODO: Your main firewall code will be here.
+        if self.handle_rules(pkt_dir pkt):
+            if pkt_dir == PKT_DIR_OUTGOING:
+                self.self.iface_ext.send_ip_packet(pkt)
+            else:
+                self.self.iface_int.send_ip_packet(pkt)
+
+    def handle_rules(self, pkt_dir, pkt):
+        #Pull all the relavant information out of the packet
+        pkt_info = self.read_pkt(pkt)
+
+        #Pass all packets that aren't using ICMP, TCP, or UDP
+        if pkt_info['protocol'] = "other":
+            return True
+
+        #Pass all DNS packets that fall outside the scope of the project
+        if pkt_info['protocol'] = "dns":
+            if pkt_info['dns_qtype'] != 1 and pkt_info['dns_qtype'] != 28:
+                return True
+            if pkt_info['dns_qclass'] != 1:
+                return True
+
+        #Handle all of the rules
+        for rule in self.rules:
+            # TODO: Do shit here to make all of the rules work :((((
+
+
+
+    def read_pkt(self, pkt):
+        #Create a dictionary to hold of the information on the packet
+        pkt_specs = {}
+        pkt_specs['valid'] = False
+
+        #If packet is too short, it is corrupted
+        if len(pkt) < 8:
+            return False
+
+        #Find the version number/Header length
+        version_and_length = struct.unpack('!B', pkt[0:1])[0]
+        pkt_specs['version'] = version_and_length >> 4
+        pkt_specs['header_len'] = version_and_length & 0b00001111
+
+        #Find the total length of the packet
+        pkt_specs['total_len'] = struct.unpack('!H', pkt[2:4])
+
+        if validate_ip(header_len, total_len):
+            #IP header is valid
+            pkt_specs['valid'] = True
+
+            pkt_specs['src_ip'] = socket.inet_ntoa(pkt[12:16])
+            pkt_specs['dst_ip'] = socket.inet_ntoa(pkt[16:20])
+            protocol_byte = struct.unpack('!B', pkt[9:10])[0]
+            pkt_specs['protocol'] = self.match_protocol(protocol_byte, pkt_specs, pkt)
+            pkt_specs['ip_id'] = struct.unpack('!H', pkt[4:6])[0]
+
+        return pkt_specs
+
+
+    def validate_ip(self, header_len, total_len):
+        #Check if header meet minimum length requirments
+        if header_len < 5:
+            return False
+        #Check if total_len matches actual packet length
+        elif total_len != len(pkt):
+            return False
+        else:
+            return True
+
+    def match_protocol(self, protocol, pkt_specs, pkt):
+        #Find the end of the IP Header in bytes
+        protocol_header = pkt_specs['header_len'] * 4
+
+        #ICMP Protocol
+        if protocol == 1:
+            pkt_specs['icmp_type'] = struct.unpack('!B', pkt[protocol_header:protocol_header + 1])[0] 
+            return "icmp"
+
+        #TCP Protocol
+        if protocol == 6:
+            pkt_info['tcp_src'] = struct.unpack('!H', pkt[protocol_header:protocol_header + 2])[0]
+            pkt_info['tcp_dst'] = struct.unpack('!H', pkt[protocol_header + 2:protocol_header + 4])[0]
+            return "tcp"
+
+        #UDP Protocol
+        if protocol == 17:
+            pkt_info['udp_src'] = struct.unpack('!H', pkt[protocol_header:protocol_header + 2])[0]
+            pkt_info['udp_dst'] = struct.unpack('!H', pkt[protocol_header + 2:protocol_header + 4])[0]
+
+            #Check if the UDP packet contains a DNS packet
+            if pckt_info['udp_dst'] == 53:
+                DNS_header = protocol_header + 8
+                QDCOUNT = struct.unpack('!H', pkt[DNS_header + 4:DNS_header + 6])[0]
+
+                if QDCOUNT != 1:
+                    #invalidate the packet
+                    pkt_specs['valid_dns'] = False
+                else:
+                    pkt_specs['valid_dns'] = True
+                    DNS_questions = DNS_header + 12
+                    pkt_specs['dns_qname'], qname_len = self.find_qname(pkt, DNS_questions)
+                    DNS_qtype_location = DNS_questions + qname_len
+                    pkt_specs['dns_qtype'] = struct.unpack('!H', pkt[DNS_qtype_location:DNS_qtype_location + 2])[0]
+                    pkt_specs['dns_qclass'] = struct.unpack('!H', pkt[DNS_qtype_location + 2:DNS_qtype_location + 4])[0]
+                return "dns"
+
+            return "udp"
+
+        return "other"
+
+
+    def find_qname(self, pkt, dns_questions):
+        domain_name = ""
+        labels = 0
+        qname_len = 0
+        dns_questions_payload[pkt[dns_questions:len(pkt)]
+
+        for byte in dns_questions_payload:
+            curr = struct.unpack('!B', dns_questions)[0]
+            #Reached the end of the domain name
+            if curr == 0:
+                break
+
+            #Just started or finished looking through the last sequence of bytes
+            elif labels != 0:
+                dns_domain += chr(curr)
+                num_labels -= 1
+
+            else:
+                labels = curr
+                qname_len = qname_len + curr + 1 #add an extra 1 to acccount for the first label
+                #Add in period between groups of labels
+                if len(domain_name) > 0:
+                    domain_name + "."
+
+        return domain_name, qname_len + 1
+
+
         pkt_src_ip = pkt[12:16]
         pkt_dst_ip = pkt[16:20]
         pkt_protocol = pkt[9:10]
